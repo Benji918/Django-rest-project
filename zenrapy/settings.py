6097f956e2bfc188cd7c21d58883a0d5c6e96ef1
Benjamin <kodiugos@gmail.com>
2022-10-28 09:01:36 -0700

    changes to settings.py

--- zenrapy/settings.py
+++ zenrapy/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    "whitenoise.runserver_nostatic",
     'account',
     'rest_framework',
     'helpers',
@@ -144,7 +144,7 @@ STATIC_URL = 'static/'
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
